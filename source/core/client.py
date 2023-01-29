1215f2fc6e4c7b42777a0332d7df3e2c58161594
Damego <danyabatueff@gmail.com>
2023-01-29 17:32:46 +0500

    refactor(client): add astart, use BotCommand

--- source/core/client.py
+++ source/core/client.py
@@ -2,6 +2,7 @@ from typing import Callable, Coroutine
 
 from aiogram import Dispatcher, Bot
 from aiogram.filters import Command
+from aiogram.types import BotCommand
 
 
 class AiogramClient:
@@ -9,11 +10,11 @@ class AiogramClient:
         self.dispatcher = Dispatcher()
         self.bot: Bot = None  # type: ignore
 
-    def command(self, name: str = None):
+    def command(self, name: str = None, description: str = "No description."):
         def wrapper(func: Callable[..., Coroutine]):
             command_name: str = name or func.__name__
 
-            self.dispatcher.message(Command(commands=[command_name]))(func)
+            self.dispatcher.message(Command(BotCommand(command=command_name, description=description)))(func)
 
         return wrapper
 
@@ -21,5 +22,6 @@ class AiogramClient:
         self.bot = Bot(token, parse_mode="HTML")
         self.dispatcher.run_polling(self.bot)
 
-
-AiogramBot = AiogramClient
+    async def astart(self, token: str):
+        self.bot = Bot(token, parse_mode="HTML")
+        await self.dispatcher.start_polling(self.bot)
