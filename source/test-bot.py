3ad054f31592a225a8851a8fbf47159a8f900ad3
Damego <danyabatueff@gmail.com>
2023-01-29 17:34:04 +0500

    refactor: update test-bot

--- source/test-bot.py
+++ source/test-bot.py
@@ -4,20 +4,45 @@ import aiogram.types
 import anilibria
 from dotenv import load_dotenv
 
-from core import AiogramBot
+from core import AiogramClient
+from core.parsers.parser import Parser
+from utils.responses import on_title_episode
+from utils.utils import build_chrome_driver
 
 load_dotenv()
 
 
-bot = AiogramBot()
+client = AiogramClient()
 anilibria_client = anilibria.AniLibriaClient()
 
+webdriver = build_chrome_driver()
+manga = Parser(webdriver, "mangalib.me")
+ranobe = Parser(webdriver, "ranobelib.me")
 
-@bot.command()
-async def random_title(message: aiogram.types.Message):
-    title = await anilibria_client.get_random_title()
-    await message.reply(title.names.ru)
+
+@anilibria_client.event()
+async def on_title_episode(event: anilibria.TitleEpisode):
+    await client.bot.send_message(724170445, on_title_episode(event))
+
+
+@anilibria_client.event()
+async def on_connect():
+    print("Connected to anilibria api")
+
+
+@client.command()
+async def manga(message: aiogram.types.Message):
+    raw = message.text.split(maxsplit=1)
+    if len(raw) == 1:
+        return await message.reply("Вы не указали название манги")
+
+    _, name = raw
+
+    data = manga.get_title(name)
+    await message.reply(data.name)
 
 
 if __name__ == "__main__":
-    bot.start(environ["TOKEN"])
+    anilibria_client.startwith(
+        client.astart(environ["TOKEN"])
+    )
